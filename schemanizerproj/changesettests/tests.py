795dd2a083028af2dec673f6242a429a1f934d12
Elmer Medez <EMedez@palominodb.com>
2013-07-10 01:31:30 +0800

    refactoring

--- schemanizerproj/changesettests/tests.py
+++ schemanizerproj/changesettests/tests.py
@@ -25,6 +25,19 @@ class ChangesetTestSyntaxTestCase(TestCase):
         self.user_dev01 = users_models.User.objects.get(name='dev01')
         self.user_dev02 = users_models.User.objects.get(name='dev02')
 
+        self.create_test_db()
+
+        self.server_test = servers_models.Server.objects.create(
+            name='test',
+            hostname=settings.TEST_DB_HOST if settings.TEST_DB_HOST else 'localhost',
+            environment=servers_models.Environment.objects.get(name='test'))
+
+        conn_opts = self.get_test_db_connection_options()
+        self.schema_version, created = schema_functions.generate_schema_version(
+            self.server_test, settings.TEST_DB_NAME,
+            connection_options=conn_opts
+        )
+
     def get_test_db_connection_options(self):
         conn_opts = {}
         if settings.TEST_DB_HOST:
@@ -35,6 +48,18 @@ class ChangesetTestSyntaxTestCase(TestCase):
             conn_opts['user'] = settings.TEST_DB_USER
         if settings.TEST_DB_PASSWORD:
             conn_opts['passwd'] = settings.TEST_DB_PASSWORD
+        return conn_opts
+
+    def get_syntax_test_connection_coptions(self):
+        conn_opts = {}
+        if settings.MYSQL_HOST:
+            conn_opts['host'] = settings.MYSQL_HOST
+        if settings.MYSQL_PORT:
+            conn_opts['port'] = settings.MYSQL_PORT
+        if settings.MYSQL_USER:
+            conn_opts['user'] = settings.MYSQL_USER
+        if settings.MYSQL_PASSWORD:
+            conn_opts['passwd'] = settings.MYSQL_PASSWORD
         return conn_opts
 
     def create_test_db(self):
@@ -50,25 +75,11 @@ class ChangesetTestSyntaxTestCase(TestCase):
         conn.close()
 
     def test_syntax_test_changeset(self):
-        self.create_test_db()
-
-        server_test = servers_models.Server.objects.create(
-            name='test',
-            hostname=settings.TEST_DB_HOST if settings.TEST_DB_HOST else 'localhost',
-            environment=servers_models.Environment.objects.get(name='test'))
-
-        conn_opts = self.get_test_db_connection_options()
-        schema_version, created = schema_functions.generate_schema_version(
-            server_test, settings.TEST_DB_NAME,
-            connection_options=conn_opts
-        )
-
         changeset = changesets_models.Changeset.objects.create(
-            database_schema=schema_version.database_schema,
+            database_schema=self.schema_version.database_schema,
             type=changesets_models.Changeset.DDL_TABLE_CREATE,
             classification=changesets_models.Changeset.CLASSIFICATION_PAINLESS
         )
-
         changeset_detail = changesets_models.ChangesetDetail.objects.create(
             changeset=changeset,
             description='create table t01',
@@ -76,20 +87,10 @@ class ChangesetTestSyntaxTestCase(TestCase):
             revert_sql='drop table t01'
         )
 
-        test_conn_opts = {}
-        if settings.MYSQL_HOST:
-            test_conn_opts['host'] = settings.MYSQL_HOST
-        if settings.MYSQL_PORT:
-            test_conn_opts['port'] = settings.MYSQL_PORT
-        if settings.MYSQL_USER:
-            test_conn_opts['user'] = settings.MYSQL_USER
-        if settings.MYSQL_PASSWORD:
-            test_conn_opts['passwd'] = settings.MYSQL_PASSWORD
-
         changeset_test_syntax = changeset_testing.ChangesetTestSyntax(
             changeset=changeset,
-            schema_version=schema_version,
-            connection_options=test_conn_opts
+            schema_version=self.schema_version,
+            connection_options=self.get_syntax_test_connection_coptions()
         )
         changeset_test_syntax.run_test()
 
@@ -101,25 +102,11 @@ class ChangesetTestSyntaxTestCase(TestCase):
         self.assertFalse(changeset_test_syntax.has_errors)
 
     def test_syntax_test_changeset_with_errors_on_apply_sql(self):
-        self.create_test_db()
-
-        server_test = servers_models.Server.objects.create(
-            name='test',
-            hostname=settings.TEST_DB_HOST if settings.TEST_DB_HOST else 'localhost',
-            environment=servers_models.Environment.objects.get(name='test'))
-
-        conn_opts = self.get_test_db_connection_options()
-        schema_version, created = schema_functions.generate_schema_version(
-            server_test, settings.TEST_DB_NAME,
-            connection_options=conn_opts
-        )
-
         changeset = changesets_models.Changeset.objects.create(
-            database_schema=schema_version.database_schema,
+            database_schema=self.schema_version.database_schema,
             type=changesets_models.Changeset.DDL_TABLE_CREATE,
             classification=changesets_models.Changeset.CLASSIFICATION_PAINLESS
         )
-
         changeset_detail = changesets_models.ChangesetDetail.objects.create(
             changeset=changeset,
             description='create table t01',
@@ -127,20 +114,10 @@ class ChangesetTestSyntaxTestCase(TestCase):
             revert_sql='drop table t01'
         )
 
-        test_conn_opts = {}
-        if settings.MYSQL_HOST:
-            test_conn_opts['host'] = settings.MYSQL_HOST
-        if settings.MYSQL_PORT:
-            test_conn_opts['port'] = settings.MYSQL_PORT
-        if settings.MYSQL_USER:
-            test_conn_opts['user'] = settings.MYSQL_USER
-        if settings.MYSQL_PASSWORD:
-            test_conn_opts['passwd'] = settings.MYSQL_PASSWORD
-
         changeset_test_syntax = changeset_testing.ChangesetTestSyntax(
             changeset=changeset,
-            schema_version=schema_version,
-            connection_options=test_conn_opts
+            schema_version=self.schema_version,
+            connection_options=self.get_syntax_test_connection_coptions()
         )
         changeset_test_syntax.run_test()
 
